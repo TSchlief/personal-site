d649df26eaba3284400552606f17d24b7525f0cb
Travis Schlief <travisschlief@hotmail.com>
2023-12-30 13:58:33 -0800

    add coinmellon project

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 const projects ={
     solar: "https://travisschlief-solar.surge.sh",
     maze: "https://tschlief.github.io/maze/",
-    blog: "https://blog-tschlief.onrender.com/"
+    blog: "https://blog-tschlief.onrender.com/",
+    coinMellon: "https://coinmellon.onrender.com/"
 };
 
 let keys = Object.keys(projects);
@@ -9,6 +10,6 @@ for(i = 0; i < keys.length; i++) {
     let key = keys[i]
     document.getElementById(keys[i]).addEventListener("click", ()=>{
         console.log(projects[keys[i]]);
-        window.location.href = projects[key];
+        window.open(projects[key], '_blank');
     });
 }
